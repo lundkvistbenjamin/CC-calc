53395b1e42b424956d31a0232df147fca44f7461
”lundkvistbenjamin” <lundkvistbenjamin@gmail.com>
2023-04-11 15:49:24 +0300

    Push

--- app/src/main/java/fi/arcada/sos_23_exempel/Statistics.java
+++ app/src/main/java/fi/arcada/sos_23_exempel/Statistics.java
@@ -1,13 +1,42 @@
 package fi.arcada.sos_23_exempel;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Statistics {
 
-    public static double calcMean(double[] arr) {
+    // Metod för att sortera data
+    public static ArrayList<Double> sortValues(ArrayList<Double> values) {
+        // vi skapar en kopia så vi inte sorterar ursprungliga datamängden
+        ArrayList<Double> sorted = new ArrayList<>(values);
+        Collections.sort(values);
+        return sorted;
+    }
+
+    public static double calcMedian(ArrayList<Double> values) {
+        ArrayList<Double> sorted = new ArrayList<>(values);
+        int middle = sorted.size()/2;
+        return sorted.get(middle);
+    }
+
+    public static double calcMean(ArrayList<Double> values) {
         double sum = 0;
-        for (int i = 0; i <= arr.length; i++){
-            sum += i;  
+        for (int i = 0; i < values.size(); i++) {
+            sum += values.get(i);
         }
-        return sum / arr.length;
+        return sum / values.size();
+    }
+
+    public static double calcSD(ArrayList<Double> values) {
+        double mean = calcMean(values);
+        double sumOffDiff = 0;
+
+        for (int i = 0; i < values.size(); i++) {
+            sumOffDiff += Math.pow(values.get(i) - mean, 2);
+        }
+        double variance = sumOffDiff / values.size();
+
+        return Math.sqrt(variance);
     }
 
 }
