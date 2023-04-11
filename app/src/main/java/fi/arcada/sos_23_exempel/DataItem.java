3b0b537f7d2a7e2f2817a8fa2a94b2129aa03dc2
”lundkvistbenjamin” <lundkvistbenjamin@gmail.com>
2023-04-11 15:50:02 +0300

    Push

---
+++ app/src/main/java/fi/arcada/sos_23_exempel/DataItem.java
@@ -0,0 +1,14 @@
+package fi.arcada.sos_23_exempel;
+
+public class DataItem {
+
+    private String name;
+
+public DataItem(String name) {
+    this.name = name;
+}
+
+    public String getName() {
+    return name;
+    }
+}
